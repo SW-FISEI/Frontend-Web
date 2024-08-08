a34da1c19dfa44e4d3b74a604c56f85f251d0cab
Christian Chico <93953404+ChristianCLop@users.noreply.github.com>
2024-08-07 19:12:20 -0500

    Implementación de NextAuth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "email", type: "email", placeholder: "test@test.com" },
+                contrasenia: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                const res = await fetch(
+                    `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
+                    {
+                        method: "POST",
+                        body: JSON.stringify({
+                            email: credentials?.email,
+                            contrasenia: credentials?.contrasenia,
+                        }),
+                        headers: { "Content-Type": "application/json" },
+                    }
+                );
+
+                if (!res.ok) {
+                    throw new Error('Credenciales inválidas');
+                }
+
+                const user = await res.json();
+                console.log(user)
+
+                if (user.error) throw user;
+
+                return user;
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            return { ...token, ...user };
+        },
+        async session({ session, token }) {
+            session.user = token as any;
+            return session;
+        },
+    },
+    pages: {
+        signIn: "/",
+    },
+});
+
+export { handler as GET, handler as POST };
