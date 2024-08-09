bca2ff7936805091359320191c37cbde2f019d2b
Eduardo <edupi1002@gmail.com>
2024-08-09 17:47:10 -0500

    Creacion e implementacion de tema personalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@
 
 import Image from "next/image";
 import LoginForm from "@/components/LoginForm";
-
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="principal">
+    <NextUIProvider>
+      <main className="uta-light text-foreground bg-background principal">
         <div className="formulario">
           <LoginForm />
         </div>
-    </main>
+      </main>
+    </NextUIProvider>
   );
 }
