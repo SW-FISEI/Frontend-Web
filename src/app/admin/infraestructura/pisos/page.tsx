14c45e8df68a1450f9d60d036ac79b265c77bdc9
Christian Chico <93953404+ChristianCLop@users.noreply.github.com>
2024-08-17 01:08:51 -0500

    Creación pagina Pisos

--- src/app/admin/infraestructura/pisos/page.tsx
+++ src/app/admin/infraestructura/pisos/page.tsx
@@ -1,8 +1,93 @@
-import React from 'react'
+"use client"
+
+import Tabla from '@/components/tabla';
+import TituloPagina from '@/components/titulo-pagina';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+
+interface Edificio {
+  id: number;
+  nombre: string;
+}
+
+interface Piso {
+  id: number;
+  nombre: string;
+  edificio: Edificio;
+}
+
+const columnas = [
+  { uid: "nombre", name: "Piso", sortable: true },
+  { uid: "edificio.nombre", name: "Edificio", sortable: true },
+  { uid: "actions", name: "Acciones" },
+];
 
 const pisos = () => {
+  const { data: session } = useSession();
+  const [piso, setPiso] = useState<Piso[]>([]);
+  const router = useRouter();
+
+  const obtenerPisos = async (nombre: string = "") => {
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pisos/buscarP`, { nombre }, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session?.user?.token}`,
+        },
+      });
+      setPiso(response.data);
+    } catch (error) {
+      console.error('Error al obtener pisos:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (session?.user?.token) {
+      obtenerPisos("");
+    }
+  }, [session]);
+
+  const handleAñadir = () => {
+    router.push('/admin/infraestructura/pisos/pisos-form');
+  }
+
+  const handleEditar = (row: Piso) => {
+    router.push(`/admin/infraestructura/pisos/pisos-form?id=${row.id}`);
+  }
+
+  const eliminarPiso = async (id: number) => {
+    try {
+      await axios.delete(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pisos/${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user?.token}`,
+          }
+        }
+      );
+      obtenerPisos("");
+    } catch (error) {
+      console.error("Error al eliminar el título:", error);
+    }
+  };
+
+  const handleEliminar = (row: Piso) => {
+    eliminarPiso(row.id);
+  };
+
   return (
-    <div>pisos</div>
+    <section className=''>
+      <TituloPagina title="Pisos" />
+      <Tabla
+        columns={columnas}
+        data={piso}
+        onEdit={handleEditar}
+        onDelete={handleEliminar}
+        onAddNew={handleAñadir}
+      />
+    </section>
   )
 }
 
---
+++ src/app/admin/infraestructura/pisos/pisos-form/page.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import axios from 'axios';
+import TituloPagina from '@/components/titulo-pagina';
+import '@/styles/formulario.scss';
+import { Input, Button, Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import { useSession } from 'next-auth/react';
+
+interface Edificio {
+    id: number;
+    nombre: string;
+}
+
+interface Piso {
+    id?: number;
+    nombre: string;
+    edificio: Edificio;
+}
+
+const PisoForm = () => {
+    const { data: session } = useSession();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const id = searchParams.get('id');
+    const [piso, setPiso] = useState<Piso>({ nombre: '', edificio: { id: 0, nombre: '' } });
+    const [edificio, setEdificio] = useState<Edificio[]>([]);
+    const [loading, setLoading] = useState(true);
+    const isEditMode = !!id;
+
+    useEffect(() => {
+        const fetchData = async (nombre: string = "") => {
+            try {
+                const aulaResponse = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/edificios/buscar`, { nombre }, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${session?.user?.token}`,
+                    },
+                });
+                setEdificio(aulaResponse.data);
+
+                if (isEditMode) {
+                    const maquinaResponse = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pisos/${id}`, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${session?.user?.token}`,
+                        },
+                    });
+
+                    setPiso({
+                        id: maquinaResponse.data.id,
+                        nombre: maquinaResponse.data.nombre,
+                        edificio: {
+                            id: maquinaResponse.data.edificio.id,
+                            nombre: maquinaResponse.data.edificio.nombre,
+                        }
+                    });
+                }
+                setLoading(false);
+            } catch (error) {
+                console.error('Error al obtener datos:', error);
+            }
+        };
+
+        if (session?.user?.token) {
+            fetchData();
+        }
+    }, [id, isEditMode, session?.user?.token]);
+
+    if (loading) {
+        return <p>Cargando...</p>;
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setPiso({ ...piso, [name]: value ?? '' });
+    };
+
+    const handleSelectChange = (name: string, value: string) => {
+        setPiso({ ...piso, [name]: value });
+    };
+
+    const handleEdificioChange = (selected: string) => {
+        const selectedEdificio = edificio.find(p => p.nombre === selected);
+        if (selectedEdificio) {
+            setPiso({ ...piso, edificio: selectedEdificio });
+        }
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const pisoDatos = {
+                nombre: piso.nombre,
+                edificio: piso.edificio.id
+            };
+
+            if (isEditMode) {
+                await axios.patch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pisos/${id}`, pisoDatos, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${session?.user?.token}`,
+                    },
+                });
+            } else {
+                await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pisos`, pisoDatos, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${session?.user?.token}`,
+                    },
+                });
+            }
+            router.push('/admin/infraestructura/pisos');
+        } catch (error) {
+            console.error('Error al guardar el piso:', error);
+        }
+    };
+
+    const handleCancel = () => {
+        router.back();
+    };
+
+    return (
+        <section className=''>
+            <TituloPagina title="Pisos" subtitle={isEditMode ? 'Editar Piso' : 'Agregar Piso'} />
+            <div className="contenedorFormulario">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <Input
+                            variant="faded"
+                            type="text"
+                            label="Piso"
+                            name="nombre"
+                            value={piso.nombre}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <Autocomplete
+                            variant="faded"
+                            label="Edificio"
+                            name="edificio"
+                            selectedKey={piso.edificio.nombre}
+                            onSelectionChange={(selected) => {
+                                const selectedValue = selected ? selected.toString() : '';
+                                handleEdificioChange(selectedValue);
+                            }}
+                            required
+                        >
+                            {edificio.map(edificio => (
+                                <AutocompleteItem key={edificio.nombre} value={edificio.nombre}>
+                                    {edificio.nombre}
+                                </AutocompleteItem>
+                            ))}
+                        </Autocomplete>
+                    </div>
+                    <div className="botonFormulario">
+                        <Button color="secondary" onPress={handleCancel}>Cancelar</Button>
+                        <Button color="primary" type="submit">
+                            {isEditMode ? 'Actualizar' : 'Agregar'}
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default PisoForm;
