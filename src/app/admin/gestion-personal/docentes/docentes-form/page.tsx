0ad97206912b0c416186e7ad6087428560e485f3
Eduardo <edupi1002@gmail.com>
2024-08-17 19:10:38 -0500

    Implementacion crud docentes

---
+++ src/app/admin/gestion-personal/docentes/docentes-form/page.tsx
@@ -0,0 +1,198 @@
+"use client";
+
+import TituloPagina from '@/components/titulo-pagina';
+import { useSession } from 'next-auth/react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Input, Button, Autocomplete, AutocompleteItem, CircularProgress } from "@nextui-org/react";
+import '@/styles/formulario.scss';
+
+interface Docente {
+    id?: number;
+    cedula: any;
+    docente: string;
+    titulo: Titulo;
+}
+
+interface Titulo {
+    id: number;
+    nombre: string;
+    abreviacion: string;
+}
+
+const DocentesForm = () => {
+    const { data: session } = useSession();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const id = searchParams.get('id');
+    const [docente, setDocente] = useState<Docente>({ cedula: '', docente: '', titulo: { id: 0, nombre: '', abreviacion: '' } });
+    const [titulos, setTitulos] = useState<Titulo[]>([]);
+    const [loading, setLoading] = useState(true);
+    const isEditMode = !!id;
+
+    const obtenerTitulos = async (nombre: string = "") => {
+        try {
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/titulos/buscarT`, { nombre }, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${session?.user?.token}`,
+                },
+            });
+            setTitulos(response.data);
+        } catch (error) {
+            console.error("Error al obtener los títulos:", error);
+        }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Obtener los títulos
+                await obtenerTitulos();
+
+                if (isEditMode) {
+                    const docenteResponse = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/docentes/${id}`, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${session?.user?.token}`,
+                        },
+                    });
+
+                    setDocente({
+                        id: docenteResponse.data.id,
+                        cedula: docenteResponse.data.cedula,
+                        docente: docenteResponse.data.docente,
+                        titulo: {
+                            id: docenteResponse.data.titulo.id,
+                            nombre: docenteResponse.data.titulo.nombre,
+                            abreviacion: docenteResponse.data.titulo.abreviacion,
+                        },
+                    });
+                }
+                setLoading(false);
+            } catch (error) {
+                console.error('Error al obtener datos:', error);
+            }
+        };
+
+        if (session?.user?.token) {
+            fetchData();
+        }
+    }, [id, isEditMode, session?.user?.token]);
+
+    if (loading) {
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <CircularProgress label="Cargando..." />
+            </div>
+        );
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setDocente({ ...docente, [name]: value });
+    };
+
+    const handleTituloChange = (selected: string) => {
+        const selectedTitulo = titulos.find(t => t.nombre === selected);
+        if (selectedTitulo) {
+            setDocente({ ...docente, titulo: selectedTitulo });
+        }
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const docenteDatos = {
+                cedula: docente.cedula,
+                docente: docente.docente,
+                titulo_id: docente.titulo.id,
+            };
+
+            if (isEditMode) {
+                await axios.patch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/docentes/${id}`, docenteDatos, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${session?.user?.token}`,
+                    },
+                });
+            } else {
+                await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/docentes`, docenteDatos, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${session?.user?.token}`,
+                    },
+                });
+            }
+            router.push('/admin/gestion-personal/docentes');
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                console.error('Error al guardar el docente:', error.response?.data);
+            } else {
+                console.error('Error inesperado:', error);
+            }
+        }
+    };
+
+    const handleCancel = () => {
+        router.back();
+    };
+
+    return (
+        <section className=''>
+            <TituloPagina title="Docentes" subtitle={isEditMode ? 'Editar Docente' : 'Agregar Docente'} />
+            <div className="contenedorFormulario">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <Input
+                            variant="faded"
+                            label="Cédula"
+                            name="cedula"
+                            value={docente.cedula}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <Input
+                            variant="faded"
+                            label="Nombre del Docente"
+                            name="docente"
+                            value={docente.docente}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <Autocomplete
+                            variant="faded"
+                            label="Titulo"
+                            name="titulo"
+                            selectedKey={docente.titulo.nombre}
+                            onSelectionChange={(selected) => {
+                                const selectedValue = selected ? selected.toString() : '';
+                                handleTituloChange(selectedValue);
+                            }}
+                            required
+                        >
+                            {titulos.map(titulo => (
+                                <AutocompleteItem key={titulo.nombre} value={titulo.nombre}>
+                                    {titulo.nombre}
+                                </AutocompleteItem>
+                            ))}
+                        </Autocomplete>
+                    </div>
+                    <div className="botonFormulario">
+                        <Button color="secondary" onPress={handleCancel}>Cancelar</Button>
+                        <Button color="primary" type="submit">
+                            {isEditMode ? 'Actualizar' : 'Agregar'}
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default DocentesForm;
--- src/app/admin/gestion-personal/docentes/page.tsx
+++ src/app/admin/gestion-personal/docentes/page.tsx
@@ -1,9 +1,104 @@
-import React from 'react'
+"use client";
 
-const docentes = () => {
-  return (
-    <div>docentes</div>
-  )
+import Tabla from '@/components/tabla';
+import TituloPagina from '@/components/titulo-pagina';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+interface Docente {
+  cedula: number;
+  docente: string;
+  titulo: Titulo;
 }
 
-export default docentes
+interface Titulo {
+  id: number;
+  nombre: string;
+  abreviacion: string;
+}
+
+const columnas = [
+  { uid: "cedula", name: "Cédula", sortable: true },
+  { uid: "titulo.nombre", name: "Titulo", sortable: true },
+  { uid: "docente", name: "Nombre", sortable: true },
+  { uid: "actions", name: "Acciones" }
+];
+
+const Docentes = () => {
+  const { data: session } = useSession();
+  const [docentes, setDocentes] = useState<Docente[]>([]);
+  const router = useRouter();
+
+  const obtenerDocentes = async (nombre: string = "") => {
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/docentes/buscar`, { nombre },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user?.token}`,
+          }
+        }
+      );
+      setDocentes(response.data);
+    } catch (error) {
+      console.error('Error al obtener docentes:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (session?.user?.token) {
+      obtenerDocentes();
+    }
+  }, [session]);
+
+  const handleAñadir = () => {
+    router.push('/admin/gestion-personal/docentes/docentes-form');
+  };
+
+  const handleEdit = (row: Docente) => {
+    router.push(`/admin/gestion-personal/docentes/docentes-form?id=${row.cedula}`);
+  };
+  
+  const handleEliminar = (row: Docente) => {
+    eliminarDocente(row.cedula);
+  };
+
+  const eliminarDocente = async (id: number) => {
+    try {
+      await axios.delete(`${process.env.NEXT_PUBLIC_BACKEND_URL}/docentes/${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user?.token}`,
+          }
+        }
+      );
+      obtenerDocentes();
+    } catch (error) {
+      console.error("Error al eliminar el docente:", error);
+    }
+  };
+
+  // Mapeo para agregar 'id' como alias de 'cedula'
+  const docentesConId = docentes.map(docente => ({
+    ...docente,
+    id: docente.cedula
+  }));
+
+  return (
+    <section className=''>
+      <TituloPagina title="Docentes" />
+      <Tabla
+        columns={columnas}
+        data={docentesConId}
+        onEdit={handleEdit}
+        onDelete={handleEliminar}
+        onAddNew={handleAñadir}
+      />
+    </section>
+  );
+}
+
+export default Docentes;
